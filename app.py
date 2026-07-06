2e061799721fcc42c95180e1facde7b276c60b25
ggovinddileep@gmail.com <you@example.com>
2026-07-06 19:55:38 +0530

    updated password hashing approach

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ credentials = {
 # Plaintext passwords for this prototype: 
 # john_doe -> "resident123"
 # water_vendor -> "delivery123"
-# The new way to hash plaintext passwords in version 0.3.x
-credentials['usernames']['john_doe']['password'] = stauth.Hasher.hash('resident123')
-credentials['usernames']['water_vendor']['password'] = stauth.Hasher.hash('delivery123')
+# Temporary simple text approach to get you past the crash:
+credentials['usernames']['john_doe']['password'] = 'resident123'
+credentials['usernames']['water_vendor']['password'] = 'delivery123'
 
 # Initialize the authenticator component
 authenticator = stauth.Authenticate(
